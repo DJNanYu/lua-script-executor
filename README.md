446703050ff64e31d8d63bedef8f64397a730346
DJNanYu <2986945687@qq.com>
2023-04-03 10:00:00 -0400

    feat: initial draft of lua-script-executor

---
+++ README.md
@@ -0,0 +1,5 @@
+# lua-script-executor
+
+Native Memory Modifier Script. 原生内存修改脚本
+
+Initial development of lua-script-executor.
---
+++ main.lua
@@ -0,0 +1,7 @@
+// Draft version for lua-script-executor
+
+
+-- 模拟用于游戏逆向或自动化测试的 Lua 脚本
+-- 修改内存中的属性或调用 Java 接口
+
+function modify_player_stats(player_id, new_score...
